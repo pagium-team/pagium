268aae5178397b4498f484ceec078e0623d4fe82
rahul <ro87630872@126.com>
2016-11-16 22:13:13 +0800

    【UPD】更新命令

--- index.js
+++ index.js
@@ -7,6 +7,21 @@ var serverCommand = require("pagium-command-server");
 
 var globalConfig = require("./config/global.js");
 var projectConfig = require("./package.json");
+var versionStr = require('./version.js');
+var imaging = require('imaging');
+
+function getVersion(){
+    imaging.draw('pagium.jpg', { width: 20}, function (resp, status) {
+        /*
+         if status == 'success', resp is the image charater string.
+         conols.log(resp) to render the image in your terminal
+         else if if status == 'fail', resp is the error message
+         */
+        (status == 'success') && console.log(resp);
+        //console.log(resp);
+    });
+}
+getVersion();
 
 /**
  * pagium 入口文件
@@ -18,7 +33,7 @@ var projectConfig = require("./package.json");
 module.exports = {
     run: function (argv) {
         program
-            .version('0.0.1')
+            .version(versionStr())
             .option('-i, --init', 'init pagium development environment ')
             .option('-r, --release', 'packing pagium program ')
             .option('-w, --watch', 'watching pagium program ')
